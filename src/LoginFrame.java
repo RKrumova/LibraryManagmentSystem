09417d5882dbc45d581a3ea9d4c64e0e10409413
Roza <stu2001321060@uni-plovdiv.bg>
2022-05-02 01:13:48 +0300

    Sign and login page are visually done and connected. You can go smoothly throught them and starting to work on next classes. Currently, the database isn't working correctly, but there is a chance this problem to be from the computer itself rather than the files.

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -1,3 +1,64 @@
-public class LoginFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class LoginFrame extends JFrame {
+    Connection conn=null;
+    PreparedStatement state=null;
+    ResultSet result=null;
+    JPanel upPanel = new JPanel();
+    JPanel downPanel = new JPanel();
+    JLabel usernameL = new JLabel("Enter username");
+    JLabel passwordL=new JLabel("Enter your password: ");
+    JTextField usernameT=new JTextField();
+    JTextField passwordT=new JTextField();
+    JButton loginButton = new JButton("Login");
+    JButton adminButton = new JButton("Admin login");
+    JButton clearLoginButton = new JButton("Clear data");
+    JButton signButton = new JButton("Sign up");
+    public LoginFrame(){
+        JPanel panel = new JPanel();
+        this.setSize(400,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(panel);
+        this.setLayout(new GridLayout(2, 1));
+        upPanel.setLayout(new GridLayout(2, 2));
+        upPanel.add(usernameL);         upPanel.add(usernameT);
+        upPanel.add(passwordL);        upPanel.add(passwordT);
+        this.add(upPanel);
+        downPanel.add(loginButton);
+        downPanel.add(adminButton);
+        downPanel.add(clearLoginButton);
+        downPanel.add(signButton);
+        loginButton.addActionListener(new StudentForm());
+        adminButton.addActionListener(new AdminForm());
+        clearLoginButton.addActionListener(new clearForm());
+        signButton.addActionListener(new GoToSign());
+        this.add(downPanel);
+        this.setVisible(true);
+    }
+    public void clearForm() {
+        usernameT.setText("");
+        passwordT.setText("");
+    }
+    class clearForm implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            clearForm();
+        }
+    }
+    class GoToSign implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            SignUpFrame signFrame = new SignUpFrame();
+            signFrame.setVisible(true);
+        }
+    }
 
 }
--- src/SignUpFrame.java
+++ src/SignUpFrame.java
@@ -2,18 +2,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 //signFrame in main
-public class SignUpFrame extends MainClass{
-
-
+public class SignUpFrame extends JFrame{
+    Connection conn=null;
+    PreparedStatement state=null;
+    ResultSet result=null;
+    int id=-1;
     //make panels
     JPanel upPanel = new JPanel();
     JPanel downPanel = new JPanel();
-    //
+    //Adding information
     JLabel fnameL = new JLabel("First name");
     JLabel lnameL=new JLabel("LastName");
     JLabel ageL = new JLabel("Age:");
@@ -23,7 +27,7 @@ public class SignUpFrame extends MainClass{
     JTextField ageT=new JTextField();
     JTextField emailT = new JTextField();
     //login information
-    JLabel usernameL = new JLabel("Enter valid username");
+    JLabel usernameL = new JLabel("Enter username");
     JLabel password1L=new JLabel("Enter your password: ");
     JLabel password2L=new JLabel("Enter your password again: ");
     JLabel secretQuestionL = new JLabel("Enter secret question:");
@@ -33,14 +37,18 @@ public class SignUpFrame extends MainClass{
     JTextField password2T=new JTextField();
     JTextField secretQuestionT = new JTextField();
     JTextField secretAnswerT = new JTextField();
-
+    //buttons
+    JButton createButton = new JButton("Create new user");
+    JButton loginButton = new JButton("Go to login");
+    JButton clearButton = new JButton("Clear data");
     public SignUpFrame (){
         //SignUpFrame signFrame = new SignUpFrame();
         JPanel panel = new JPanel();
-        signFrame.setSize(400,600);
-        signFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        signFrame.add(panel);
-        upPanel.setLayout(new GridLayout(5, 2));
+        this.setSize(400,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(panel);
+        this.setLayout(new GridLayout(2, 1));
+        upPanel.setLayout(new GridLayout(8, 2));
         upPanel.add(fnameL);            upPanel.add(fnameT);
         upPanel.add(lnameL);            upPanel.add(lnameT);
         upPanel.add(ageL);              upPanel.add(ageT);
@@ -49,6 +57,91 @@ public class SignUpFrame extends MainClass{
         upPanel.add(password2L);        upPanel.add(password2T);
         upPanel.add(secretQuestionL);   upPanel.add(secretQuestionT);
         upPanel.add(secretAnswerL);     upPanel.add(secretAnswerT);
-        signFrame.setVisible(true);
+        this.add(upPanel);
+        //downPanel
+        downPanel.add(createButton);
+        createButton.addActionListener(new AddAction());
+        downPanel.add(loginButton);
+        loginButton.addActionListener(new GoToLogin());
+        downPanel.add(clearButton);
+        clearButton.addActionListener(new ClearAction());
+        //clearButton.addActionListener(new SignUpFrame.clearForm());
+        this.add(downPanel);
+        this.setVisible(true);
+    }
+    public void clearForm() {
+        fnameT.setText("");
+        lnameT.setText("");
+        ageT.setText("");
+        emailT.setText("");
+        usernameT.setText("");
+        password1T.setText("");
+        password2T.setText("");
+        secretQuestionT.setText("");
+        secretAnswerT.setText("");
+    }
+    class AddAction implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            conn = DBConnection.getConnection();
+            String sql = "Insert into userdata(fname,lname,age, username,password, question, answer)" + "values(?,?,?,?,?,?,?)";
+            try {
+                state=conn.prepareStatement(sql);
+                state.setString(1,fnameT.getText());
+                state.setString(2,lnameT.getText());
+                state.setInt(3,Integer.parseInt(ageT.getText()));
+                state.setString(4, usernameT.getText());
+                state.setString(5, password1T.getText());
+                state.setString(6, secretQuestionT.getText());
+                state.setString(7, secretAnswerT.getText());
+                state.execute();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    class ClearAction implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            clearForm();
+        }
+    }
+    class GoToLogin implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            LoginFrame loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
+        }
+    }
+    class MouseAction implements MouseListener{
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            //int
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 }
